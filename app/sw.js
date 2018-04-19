8e4038111f3cce01b513c9aed95400b09c3ca08b
sgluzins <sgluzins@gmail.com>
2018-04-19 14:50:39 -0700

    adding sw file

---
+++ app/sw.js
@@ -0,0 +1,29 @@
+importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', (e) => {
+ e.waitUntil(
+   caches.open('sgluzins').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/stylesheet.css',
+       '/assets/*'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', () => {
+    console.log(event.request.url);
+    event.respondWith(caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+    }));
+});
+
+<script>
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then( () =>
+        console.log("service worker registered!")
+    )}
+)}
+</script>
