0b1a02eea4545f2118b747f93b770e93ac0f03ae
sgluzins <sgluzins@gmail.com>
2018-04-19 15:11:50 -0700

    removing arrow functions

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-self.addEventListener('install', (e) => {
+self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('sgluzins').then(function(cache) {
      return cache.addAll([
@@ -11,7 +11,7 @@ self.addEventListener('install', (e) => {
  );
 });
 
-self.addEventListener('fetch', () => {
+self.addEventListener('fetch', function() {
     console.log(event.request.url);
     event.respondWith(caches.match(event.request).then(function(response) {
         return response || fetch(event.request);
